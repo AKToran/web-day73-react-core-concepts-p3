deb96ebf027bed2ee73c77660a13456caf416d55
AKToran <aktoran60@gmail.com>
2025-04-19 11:14:51 +0600

    making custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import ControlledField from './Components/ControlledField/ControlledField'
 import FormAction from './Components/FormAction/FormAction'
+import HookForm from './Components/HookForm/HookForm'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 import Uncontrolled from './Components/Uncontrolled/Uncontrolled'
 
@@ -12,7 +13,8 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <FormAction></FormAction> */}
       {/* <ControlledField></ControlledField> */}
-      <Uncontrolled></Uncontrolled>
+      {/* <Uncontrolled></Uncontrolled> */}
+      <HookForm></HookForm>
     </div>
   )
 }
---
+++ src/Components/HookForm/HookForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import useInputField from '../Hooks/useInputField';
+
+const HookForm = () => {
+  const [name, nameOnChange] = useInputField('');
+  const [email, emailOnChange] = useInputField('');
+  const [password, passwordOnChange] = useInputField('');
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("submit", name, email);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input onChange={nameOnChange} value={name} type="text" name="name" id="name" placeholder='name' />
+        <br />
+        <input onChange={emailOnChange} value={email}
+        placeholder='email' type="email" name="email" id="email" />
+        <br />
+        <input onChange={passwordOnChange} value={password} type="password" name="password" id="password" placeholder='password' />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default HookForm;
